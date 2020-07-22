79b1114f9f18da0feccabfb4b5dd2d38cb391971
chenziqiang <chenziqiang@cszysoft.com>
2020-07-22 22:56:42 +0800

    FrameUtil 初始化修改

--- framework/src/main/java/com/android/framework/uitls/FrameUtil.java
+++ framework/src/main/java/com/android/framework/uitls/FrameUtil.java
@@ -1,10 +1,29 @@
 package com.android.framework.uitls;
 
-import android.content.Context;
+import android.app.Application;
 
+import com.lzy.okgo.OkGo;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import org.litepal.LitePal;
+
+/**
+ * 简单的初始化
+ *
+ * @author 陈自强
+ */
 public class FrameUtil {
-    public static void init(Context context) {
+    public static void init(Application context, final boolean debug) {
         ViewTools.init(context);
         SPUtils.getInstance(context);
+        LitePal.initialize(context);
+        OkGo.getInstance().init(context);
+        Logger.addLogAdapter(new AndroidLogAdapter() {
+            @Override
+            public boolean isLoggable(int priority, String tag) {
+                return debug;
+            }
+        });
     }
 }
